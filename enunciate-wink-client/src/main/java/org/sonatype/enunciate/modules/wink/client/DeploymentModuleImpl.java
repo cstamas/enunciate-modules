a7d23a0127e0773ec1629cf9d4c7319c6ce81a8e
Jason Dillon <jason@planet57.com>
2010-03-26 20:32:20 -0700

    Add basic deployment module muck

---
+++ enunciate-wink-client/src/main/java/org/sonatype/enunciate/modules/wink/client/DeploymentModuleImpl.java
@@ -0,0 +1,22 @@
+package org.sonatype.enunciate.modules.wink.client;
+
+import freemarker.template.TemplateException;
+import org.codehaus.enunciate.EnunciateException;
+import org.codehaus.enunciate.modules.FreemarkerDeploymentModule;
+
+import java.io.IOException;
+
+/**
+ * ???
+ *
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @since 0.1
+ */
+public class DeploymentModuleImpl
+    extends FreemarkerDeploymentModule
+{
+    @Override
+    public void doFreemarkerGenerate() throws EnunciateException, IOException, TemplateException {
+        // TODO:
+    }
+}
